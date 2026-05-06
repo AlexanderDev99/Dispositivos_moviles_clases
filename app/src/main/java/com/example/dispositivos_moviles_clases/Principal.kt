582afdc443ccede4dcc23d4752507cec394bbec1
AlexanderDev <alexander@uce.edu.ec>
2026-05-06 18:44:45 -0500

    clase de urls 1

--- app/src/main/java/com/example/dispositivos_moviles_clases/Principal.kt
+++ app/src/main/java/com/example/dispositivos_moviles_clases/Principal.kt
@@ -1,5 +1,7 @@
 package com.example.dispositivos_moviles_clases
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +10,31 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.dispositivos_moviles_clases.databinding.ActivityPrincipalBinding
 
 class Principal : AppCompatActivity() {
-    lateinit var binding: ActivityPrincipalBinding
+    private lateinit var binding: ActivityPrincipalBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityPrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initListeners()
     }
+
+    private fun initListeners() {
+        binding.urlBtn.setOnClickListener {
+            val url = binding.urlText.text.toString()
+
+
+            //CREACION DE LA URI
+
+            //creamos el action view
+            val i = Intent(Intent.ACTION_VIEW)
+            //transformamos la url a una uri
+            i.setData(Uri.parse(url))
+            startActivity(i) //inciamos la activity
+
+        }
+    }
+
 
 }
