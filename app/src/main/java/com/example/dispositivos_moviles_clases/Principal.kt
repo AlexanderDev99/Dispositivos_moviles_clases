99884594ab04d606d110016e2348a52dbead7ccb
AlexanderDev <Alexander@gmail.com>
2026-05-06 19:20:30 -0500

    Clase de urls y manejo de apps externas 2

--- app/src/main/java/com/example/dispositivos_moviles_clases/Principal.kt
+++ app/src/main/java/com/example/dispositivos_moviles_clases/Principal.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dispositivos_moviles_clases.databinding.ActivityPrincipalBinding
+import com.google.android.material.snackbar.Snackbar
 
 class Principal : AppCompatActivity() {
     private lateinit var binding: ActivityPrincipalBinding
@@ -17,7 +18,21 @@ class Principal : AppCompatActivity() {
         binding = ActivityPrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initVariables()
         initListeners()
+    }
+
+    private fun initVariables() {
+        //let evita valores nulos con
+        // ? decimos que si existe tomalo y si no lo haces nada
+        intent.extras.let {
+            var saludo = it?.getString("xx1")
+            Snackbar.make(
+                binding.urlText,
+                saludo.toString(),
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
     }
 
     private fun initListeners() {
@@ -27,11 +42,22 @@ class Principal : AppCompatActivity() {
 
             //CREACION DE LA URI
 
+
+          /*
             //creamos el action view
             val i = Intent(Intent.ACTION_VIEW)
             //transformamos la url a una uri
             i.setData(Uri.parse(url))
             startActivity(i) //inciamos la activity
+*/
+
+
+            //llamada a un mapa
+            val gmmIntentUri = Uri.parse("geo:37.7749,-122.4194")
+            val mapIntent = Intent(Intent.ACTION_VIEW)
+            mapIntent.setData(gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
 
         }
     }
